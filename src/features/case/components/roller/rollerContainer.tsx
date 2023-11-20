33eb229a62f641cbe8baa51d247d9e382de81955
vitoreow <vitoradriel66@gmail.com>
2023-11-20 19:28:44 -0300

    fix rollerContainer

--- src/features/case/components/roller/rollerContainer.tsx
+++ src/features/case/components/roller/rollerContainer.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from "react";
+import { HTMLAttributes } from "react";
 
-export function RollerContainer(props: any) {
+export function RollerContainer(props: HTMLAttributes<HTMLDivElement>) {
 	return (
 		<div className="relative w-[1000px] h-[100px] border border-black overflow-hidden">
 			{props.children}
