c5fc2d02ca0a3dacb01a461ef1a80fcdb0f011ad
vitoreow <vitoradriel66@gmail.com>
2023-12-13 10:13:55 -0300

    show inverted items on crate desc

--- src/features/crate/components/crateDesc/index.tsx
+++ src/features/crate/components/crateDesc/index.tsx
@@ -4,11 +4,12 @@ import { CrateItem } from "./crateItem";
 import { ItemsSkeleton } from "./itemsSkeleton";
 
 export default function CrateDesc({ items }: { items: SkinType[] }) {
+	const invertedItems = [...items].reverse();
 	return (
 		<div>
-			{items.length > 0 ? (
+			{invertedItems.length > 0 ? (
 				<div className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3 min-[320px]:grid-cols-2">
-					{items.map(item => {
+					{invertedItems.map(item => {
 						return (
 							<div key={item.name}>
 								<CrateItem
