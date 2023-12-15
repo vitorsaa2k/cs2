31d803ae6cf552ed6b5db5f9e97845b20c0d819b
vitoreow <vitoradriel66@gmail.com>
2023-12-15 19:01:09 -0300

    fix crateItem missing

--- src/features/crate/components/crateDesc/crateItem.tsx
+++ src/features/crate/components/crateDesc/crateItem.tsx
@@ -1,24 +1,24 @@
 import { parseItemNameExterior } from "../../../../utils/crate/parseItemName";
+import { SkinType } from "../../types/api";
 
-export function CrateItem({
-	name,
-	price,
-	img,
-}: {
-	name: string;
-	price: number;
-	img: string;
-}) {
-	const itemName = parseItemNameExterior(name);
+export function CrateItem({ item }: { item: SkinType }) {
+	const itemName = parseItemNameExterior(item.name);
+	const itemChance = ((item.maxRate - item.minRate) / 10000).toFixed(2);
 	return (
-		<div className="bg-zinc-600 rounded m-3 flex flex-col items-center w-[160px] justify-around text-white">
+		<div className="bg-zinc-600 rounded relative m-3 flex flex-col items-center w-[160px] justify-around text-white">
 			<div className="flex flex-col items-center">
 				<div className="text-sm">{itemName[0]}</div>
 				<div className="text-sm">{itemName[1]}</div>
 				<div className="text-sm">{itemName[2]}</div>
-				<div>${price}</div>
+				<div>${item.price}</div>
 			</div>
-			<img className="max-w-[128px]" src={img} />
+			<p className="bottom-0 left-0 p-1 absolute bg-black/30 rounded-se">
+				{itemChance}%
+			</p>
+			<img
+				className="max-w-[128px]"
+				src={`https://steamcommunity-a.akamaihd.net/economy/image/${item.icon_url}`}
+			/>
 		</div>
 	);
 }
