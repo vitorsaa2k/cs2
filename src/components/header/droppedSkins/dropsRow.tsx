1313ef0c67a39a2185b05b9139f1ccfceae7243b
vitoreow <vitoradriel66@gmail.com>
2023-12-18 22:52:22 -0300

    fix build errors

--- src/components/header/droppedSkins/dropsRow.tsx
+++ src/components/header/droppedSkins/dropsRow.tsx
@@ -2,13 +2,11 @@ import { Drop } from "./singleDrop";
 import { RecentDropsProps } from "./types";
 
 export function DropsRow(props: RecentDropsProps) {
-    return( 
-        <div className="flex flex-row-reverse overflow-hidden" >
-            {props.items.map((value, index)=>{
-                 return(
-                 <Drop name={value}/>
-                 )
-            })}
-    </div>
-    )
+	return (
+		<div className="flex flex-row-reverse overflow-hidden">
+			{props.items.map(value => {
+				return <Drop name={value} />;
+			})}
+		</div>
+	);
 }
