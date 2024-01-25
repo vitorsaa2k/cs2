f3a6558235542967efa59b56e3e0686caf4b64a2
vitoreow <vitoradriel66@gmail.com>
2024-01-25 10:44:12 -0300

    create skinImage component

---
+++ src/components/skin/skinImage.tsx
@@ -0,0 +1,15 @@
+import { HTMLAttributes } from "react";
+import { SkinType } from "../../features/crate/types/api";
+
+interface SkinImageProps extends HTMLAttributes<HTMLDivElement> {
+	skin: SkinType;
+}
+
+export function SkinImage(props: SkinImageProps) {
+	return (
+		<img
+			{...props}
+			src={`https://steamcommunity-a.akamaihd.net/economy/image/${props.skin.icon_url}`}
+		/>
+	);
+}
