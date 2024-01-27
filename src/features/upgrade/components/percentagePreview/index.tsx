b8c23ff592778c111d6a1a9d2a7c2f4193c4905f
vitoreow <vitoradriel66@gmail.com>
2024-01-27 14:54:38 -0300

    add percentage preview updates

--- src/features/upgrade/components/percentagePreview/index.tsx
+++ src/features/upgrade/components/percentagePreview/index.tsx
@@ -1,12 +1,29 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { UpgradeContext } from "../../../../contexts/upgradeContext";
+import { getUpgradeChance } from "../../../../services/upgradeApi";
+import { ActionTypes } from "../../contextTypes";
 
 export function PercentagePreview() {
 	const upgradeContext = useContext(UpgradeContext);
+
+	useEffect(() => {
+		getUpgradeChance(
+			upgradeContext.state.userSkins,
+			upgradeContext.state.skinsUpgrade
+		).then(data =>
+			upgradeContext.dispatch({
+				type: ActionTypes.UPDATE_SUCCESS_CHANCE,
+				payload: data > 100 ? 100 : data,
+			})
+		);
+		//disabling eslint due to specific needs
+		//eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [upgradeContext.state.skinsUpgrade, upgradeContext.state.userSkins]);
+
 	return (
 		<div className="flex items-center">
 			<div className="w-[10rem] h-[10rem] bg-white rounded-full"></div>
-			<p>{upgradeContext.state.successChance}</p>
+			<p>{upgradeContext.state.successChance?.toFixed(2)}</p>
 		</div>
 	);
 }
