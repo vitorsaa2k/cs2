e5168d0ba462a0012ef3d932c1cd8420a0c2305b
vitoreow <vitoradriel66@gmail.com>
2024-01-27 15:06:31 -0300

    create controlSection

---
+++ src/features/upgrade/components/controlSection.tsx
@@ -0,0 +1,10 @@
+import { UpgradeButton } from "./upgradeButton";
+
+export function ControlSection() {
+	return (
+		<div className="flex justify-around m-4">
+			<div></div>
+			<UpgradeButton />
+		</div>
+	);
+}
--- src/pages/Upgrade.tsx
+++ src/pages/Upgrade.tsx
@@ -1,4 +1,5 @@
 import { UpgradeContextProvider } from "../contexts/upgradeContext";
+import { ControlSection } from "../features/upgrade/components/controlSection";
 import { SkinsSection } from "../features/upgrade/components/skinsSection";
 import { UpgradeSection } from "../features/upgrade/components/upgradeSection";
 
@@ -6,6 +7,7 @@ export function Upgrade() {
 	return (
 		<UpgradeContextProvider>
 			<UpgradeSection />
+			<ControlSection />
 			<SkinsSection />
 		</UpgradeContextProvider>
 	);
