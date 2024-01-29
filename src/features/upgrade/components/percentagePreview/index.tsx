b682c9f53fbf8590ee8879f5d68d830953dc0aaf
vitoreow <vitoradriel66@gmail.com>
2024-01-29 13:51:50 -0300

    add semi circle

--- src/features/upgrade/components/percentagePreview/index.tsx
+++ src/features/upgrade/components/percentagePreview/index.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { UpgradeContext } from "../../../../contexts/upgradeContext";
 import { getUpgradeChance } from "../../../../services/upgradeApi";
 import { ActionTypes } from "../../contextTypes";
+import { Circle } from "./circle";
 
 export function PercentagePreview() {
 	const upgradeContext = useContext(UpgradeContext);
@@ -10,23 +11,20 @@ export function PercentagePreview() {
 		getUpgradeChance(
 			upgradeContext.state.userSkins,
 			upgradeContext.state.skinsUpgrade
-		).then(data =>
+		).then(data => {
 			upgradeContext.dispatch({
 				type: ActionTypes.UPDATE_SUCCESS_CHANCE,
 				payload: data > 100 ? 100 : data,
-			})
-		);
+			});
+		});
 		//disabling eslint due to specific needs
 		//eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [upgradeContext.state.skinsUpgrade, upgradeContext.state.userSkins]);
 
 	return (
-		<div className="flex items-center">
-			<div className="w-[12rem] h-[12rem] border flex items-center justify-center rounded-full">
-				<p className="">
-					{upgradeContext.state.successChance?.toFixed(2) ?? 0}%
-				</p>
-			</div>
+		<div className="flex relative items-center">
+			<Circle chance={upgradeContext.state.successChance} />
+			<div className="rounded-full"></div>
 		</div>
 	);
 }
---
+++ src/features/upgrade/components/triangle/triangleSVG.tsx
@@ -0,0 +1,13 @@
+export function TriangleSVG() {
+	return (
+		<svg
+			className="fill-white rotate-180"
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+		>
+			<path d="M24 22h-24l12-20z" />
+		</svg>
+	);
+}
