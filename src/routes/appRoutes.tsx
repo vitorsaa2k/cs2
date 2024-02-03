1cfb9367e8fbccd5969deb135ec0588828f7b6af
vitoreow <vitoradriel66@gmail.com>
2024-02-03 08:54:36 -0300

    remove dashboard stuff from main website

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Profile } from "../pages/Profile";
 import { Crate } from "../pages/Crate";
-import { Dashboard } from "../pages/Dashboard";
 import { ProvablyFair } from "../pages/ProvablyFair";
 import { PublicProfile } from "../pages/PublicProfile";
 import { Upgrade } from "../pages/Upgrade";
@@ -16,7 +15,6 @@ export function AppRoutes() {
 			<Route path="/crate/:name" element={<Crate />} />
 			<Route path="/provablyfair" element={<ProvablyFair />} />
 			<Route path="/upgrade" element={<Upgrade />} />
-			<Route path="/dashboard" element={<Dashboard />} />
 		</Routes>
 	);
 }
