de3051464377ae7501b2537bbe08756b50ba903e
vitoreow <vitoradriel66@gmail.com>
2024-03-07 17:12:08 -0300

    add navItem styles from figma

--- src/components/header/nav/navItem.tsx
+++ src/components/header/nav/navItem.tsx
@@ -1,9 +1,18 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, ReactNode } from "react";
 
 type NavItemProps = HTMLAttributes<HTMLDivElement> & {
 	href: string;
+	icon?: ReactNode;
 };
 
 export function NavItem(props: NavItemProps) {
-	return <a href={props.href}>{props.children}</a>;
+	return (
+		<a
+			className="bg-slate-900 flex gap-2 drop-shadow-[0_4px_25px_rgba(0,0,0,0.25)] px-10 py-2 rounded-full"
+			href={props.href}
+		>
+			{props.icon}
+			{props.children}
+		</a>
+	);
 }
