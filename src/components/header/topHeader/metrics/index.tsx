3d9db7ef21852e23bd8884551b8256402276f7c8
Vitor Adriel <vitoradriel66@gmail.com>
2024-03-30 14:37:34 -0300

    make metrics responsive

--- src/components/header/topHeader/metrics/index.tsx
+++ src/components/header/topHeader/metrics/index.tsx
@@ -6,7 +6,7 @@ import { TotalUpgrades } from "./totalUpgrades";
 
 export function Metrics() {
 	return (
-		<div className="flex flex-col gap-5 md:flex-row absolute ml-[17rem] mt-[0.2rem]">
+		<div className="hidden gap-5 xl:flex absolute ml-[17rem] mt-[0.2rem]">
 			<PlayersOnline />
 			<TotalAccounts />
 			<TotalBattles />
