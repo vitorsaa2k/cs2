f1ff2162f1ea4182adf3f0185c90a1b45797316b
vitorsaa2k <vitoradriel66@gmail.com>
2025-01-09 16:28:37 -0300

    add background color, fixed width, only 5 crates column.

--- src/features/sections/components/row.tsx
+++ src/features/sections/components/row.tsx
@@ -3,9 +3,9 @@ import { CrateInfo } from "./crateInfo";
 
 export function ProductRow({ section }: { section: Section }) {
 	return (
-		<div className="m-4 flex flex-col gap-4">
-			<p>{section.name}</p>
-			<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
+		<div className="m-4 w-[1120px] py-6 flex flex-col justify-center items-center bg-slate-950 rounded-2xl gap-4">
+			<p className="text-2xl font-semibold">{section.name}</p>
+			<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2">
 				{section.crates.map(crate => {
 					return (
 						<CrateInfo
