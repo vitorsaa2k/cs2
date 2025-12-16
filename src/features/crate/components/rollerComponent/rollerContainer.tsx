40cbd8b8cf4372fcd979acd1a8b973f0ac641134
vitorsaa2k <vitoradriel66@gmail.com>
2025-12-16 11:01:56 -0300

    add overflow-hidden to roller component

--- src/features/crate/components/rollerComponent/rollerContainer.tsx
+++ src/features/crate/components/rollerComponent/rollerContainer.tsx
@@ -2,7 +2,7 @@ import { RollerInside } from "./rollerInside";
 
 export function RollerContainer() {
 	return (
-		<div className="flex max-w-sm xl:max-w-7xl sm:max-w-2xl gap-1 overflow-hidden border-[20px] border-slate-800 rounded bg-slate-800 drop-shadow-[4px_4px_24px_rgba(0,0,0,0.25)]">
+		<div className="flex w-[1280px] overflow-hidden border-[20px] border-slate-800 rounded bg-slate-800 drop-shadow-[4px_4px_24px_rgba(0,0,0,0.25)]">
 			<RollerInside />
 		</div>
 	);
