a53e9fd01aeb546420dd490ec333eaf510eac997
vitorsaa2k <vitoradriel66@gmail.com>
2026-01-12 21:20:32 -0300

    fix sections responsivity

--- src/features/sections/components/row.tsx
+++ src/features/sections/components/row.tsx
@@ -3,9 +3,9 @@ import { CrateInfo } from "./crateInfo";
 
 export function ProductRow({ section }: { section: Section }) {
 	return (
-		<div className="m-4 w-[1120px] py-6 flex flex-col justify-center items-center bg-slate-950 rounded-2xl gap-4">
+		<div className="flex flex-col justify-center items-center bg-slate-950 sm:rounded-2xl p-3 gap-4 w-full max-w-[900px]">
 			<p className="text-2xl font-semibold">{section.name}</p>
-			<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2">
+			<div className="flex flex-wrap max-md:justify-center gap-3 w-full">
 				{section.crates.map(crate => {
 					return (
 						<CrateInfo
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,7 @@ import { Sections } from "../features/sections";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center">
-			{" "}
+		<div className="flex flex-col items-center justify-center gap-3 my-4">
 			<Sections />
 		</div>
 	);
